e83da66838ea20ccfb1452f4ee80d193ee5a2b3c
Roene <afc-ajax-roene@live.nl>
2018-10-18 16:03:24 +0200

    JS beautify

--- public/assets/general.js
+++ public/assets/general.js
@@ -1,19 +1,19 @@
 function number(element) {
 
-	var values = document.querySelectorAll('.choose option:checked');
-	var totalWeight = element.options[element.selectedIndex].value;
-	for (var i = 0; i < values.length; i++) {
-		var weight = values[i].getAttribute('value');
-		var number = parseFloat(weight);
-		factorRisk(number);
-	}
-	calculateRisk(Number(totalWeight.replace(',', '.')));
+    var values = document.querySelectorAll('.choose option:checked');
+    var totalWeight = element.options[element.selectedIndex].value;
+    for (var i = 0; i < values.length; i++) {
+        var weight = values[i].getAttribute('value');
+        var number = parseFloat(weight);
+        factorRisk(number);
+    }
+    calculateRisk(Number(totalWeight.replace(',', '.')));
 }
 
 function calculateRisk(X) {
-  var riskNumber = Number((1 / (1 + Math.exp(-1 * (-8.57219 + X))) * 10000).toFixed(2));
-  document.getElementById('risk_num').innerHTML = riskNumber + '%';
-  return riskNumber;
+    var riskNumber = Number((1 / (1 + Math.exp(-1 * (-8.57219 + X))) * 10000).toFixed(2));
+    document.getElementById('risk_num').innerHTML = riskNumber + '%';
+    return riskNumber;
 }
 
 function factorRisk() {
